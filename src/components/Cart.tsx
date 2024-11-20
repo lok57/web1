b751d558172292830a07a7b9cecf8223b8415d1b
lok57 <lokeshyadav19113@gmail.com>
2024-11-21 01:44:57 +0530

    Update Cart.tsx

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,5 +1,3 @@
-// src/components/Cart.tsx
-
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { CartItem } from '../types'; // Ensure CartItem type is correctly imported
